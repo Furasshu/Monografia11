0e7d09517cd48b904ce083f04356f8ad5e0a2b35
Furasshu <61592545+Furasshu@users.noreply.github.com>
2022-10-06 13:53:41 -0500

    Add files via upload

--- Monografía 11/website/auth.py
+++ Monografía 11/website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html", boolean=True)
 
@@ -10,6 +10,21 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        if len(email) < 4:
+            flash('El correo debe tener mas de 3 caracteres.', category='error')
+        elif len(first_name) < 2:
+            flash('El primer nombre debe tener mas de 1 caracter.', category='error')
+        elif password1 != password2:
+            flash('Las contraseñas no coinciden.', category='error')
+        elif len(password1) < 7:
+            flash('La contraseña debe tener al menos 7 caracteres.', category='error')
+        else:
+            flash('Cuenta creada!', category='success')
     return render_template("sign_up.html")
